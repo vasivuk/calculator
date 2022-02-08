90909f900b45544a6cfbb16b3ed533903e7e9330
vasivuk <vasivuk@yahoo.com>
2022-02-08 18:01:23 +0100

    Added some functionality, still a prototype

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+const numbers = document.querySelectorAll('.number');
+const screen = document.querySelector('.screen');
+const equals = document.querySelector('.equals');
+const operations = document.querySelectorAll('.operation');
 
+for(const number of numbers){
+    number.addEventListener('click', () => {
+        screen.textContent += number.value;
+    })
+}
+
+let Expression = {};
+
+//Clicking the operation
+for(const operation of operations) {
+    operation.addEventListener('click', () => {
+        Expression.firstOperand = parseInt(screen.textContent);
+        Expression.operation = operation.value;
+        screen.textContent = '';
+    });
+}
+
+equals.addEventListener('click', () => {
+    Expression.secondOperand = parseInt(screen.textContent);
+    let result = operate(Expression.operation, Expression.firstOperand, Expression.secondOperand);
+    screen.textContent = result;
+})
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => screen.textContent = '');
+
+// const Expression = {
+//     firstOperand: null,
+//     operation: null,
+//     secondOperand: null
+// }
 
 function add(num1, num2){
     return num1 + num2;
