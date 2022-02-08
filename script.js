d1f30221ba3b4c572f3922d02ab32b934ec221dc
vasivuk <vasivuk@yahoo.com>
2022-02-08 14:02:44 +0100

    Calculator functions added

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return Math.floor(num1/num2 *1000)/1000;
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+    }
+}
